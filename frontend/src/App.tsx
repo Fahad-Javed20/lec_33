4e1f45df01682cff83029853bbdcefd4cc18fb61
Fahad Bin Javed <126807388+Fahad-Javed20@users.noreply.github.com>
2026-01-03 13:39:02 +0500

    make chnages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import CounterComponent from "./components/CounterComponent";
 import LanguageComponent from "./components/LanguageComponent";
+import Button from "./ui/Button";
+
 
 function App() {
   return (
     <>
-    <div className="flex flex-col gap-10 items-center">
-
+    <div className="flex flex-col gap-10 items-center mt-20">
+      <Button />
       <CounterComponent />
       <LanguageComponent />
     </div>
---
+++ frontend/src/ui/Button.tsx
@@ -0,0 +1,20 @@
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import DeleteIcon from "@mui/icons-material/Delete";
+import SendIcon from "@mui/icons-material/Send";
+
+const IconLabelButtons = () => {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button variant="outlined" startIcon={<DeleteIcon />}>
+        Delete
+      </Button>
+
+      <Button variant="contained" endIcon={<SendIcon />}>
+        Send
+      </Button>
+    </Stack>
+  );
+};
+
+export default IconLabelButtons;
